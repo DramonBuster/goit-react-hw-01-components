3ff91fbe37e911e18d862e26815dce13bed232b5
DramonBuster <terradestroyman@gmail.com>
2021-11-29 18:02:13 +0200

    Начальные файлы

---
+++ src/index.js
@@ -0,0 +1,40 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+// ================================
+
+// import React from "react";
+// import ReactDOM from 'react-dom';
+
+// const el1 = <span>Hello </span>;
+// const el2 = <span>world</span>;
+// const jsxElement = (
+//   <div>
+//     {el1}
+//     {el2}
+//   </div>
+// );
+// ReactDOM.render(jsxElement, document.querySelector('#root'));
+
+// console.log(jsxElement);
+// ================================
+
+import ReactDOM from 'react-dom';
+
+const jsxElement = <div></div>;
+
+ReactDOM.render(jsxElement, document.querySelector('#root'));
